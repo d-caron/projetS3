c64741c2f3b9f15e650de8b875a715fc22a848c2
Dylan CARON <dcaron81.dc@gmail.com>
2019-01-03 16:05:45 +0100

    Ajout du code des fonctions de liste.x + commentary

--- head/liste.h
+++ head/liste.h
@@ -1,37 +1,233 @@
-/* liste statique */
-/* Vincent Dugat 2018 */
+/**
+ * @file liste.h
+ *
+ * @brief Fichier de déclaration relative aux listes
+ *
+ * Déclare un nouveau type de structure permettant d'encapsuler les listes d'arcs
+ * et un enssemble de fonctions permettant de manipuler ces dernières
+ *
+ * @author Vincent DUGAT, Dylan CARON
+ *
+ * @version 0.0.15
+ *
+ * @date 03/01/2019 
+ */
+
 
 #ifndef listeSTAT
 #define listeSTAT
 
+
 #include "elementliste.h"
-#define DIMMAX 100 // taille maximale de la liste statique
-#define VIDE -1   // constante  pour indiquer que la liste est vide (par convention)
 
-/* ----------------------------------
-        Déclaration des types
-   --------------------------------*/
 
-struct liste {
+#define DIMMAX 100 /// @def taille maximale de la liste statique
+#define VIDE -1   /// @def constante  pour indiquer que la liste est vide (par convention)
+
+
+/**
+ * @struct liste
+ *
+ * @brief Liste d'arcs
+ *
+ * Liste d' Elementliste de dimension DIMMAX où 
+ * Tete correspond à la première case de la liste et 
+ * nbElt au nombre d'élément qu'elle contient
+ */
+typedef struct liste {
     Elementliste Tabliste[DIMMAX];
     int Tete;
     int nbElt;
-};
+} liste;
 
-typedef struct liste liste;
-int nbEltList(liste lst); // nombre d'éléments
-void createList(liste *p); // renvoie une liste statique
-void addFrontList(liste *p, Elementliste e); // ajoute un élément devant
-void addTailList(liste *p, Elementliste e); // ajoute un élément derrière
-void delTailList(liste *p); // supprime l'élément de fin
-void delFrontList(liste *p); // supprime l'élément de tête
-void headList(liste p, Elementliste *e); // renvoie l'élément de tête
-void tailList(liste p, Elementliste *e); // renvoie l'élément de fin
-bool emptyList(liste p); // renvoie true si la pile est vide, false sinon
-bool fullList(liste p); // renvoie true si la pile est pleine, false sinon
-void dumpList(liste p,FILE *fp); // affiche le contenu de la liste
-void swapEltList(Elementliste *a,Elementliste *b); // échange deux éléments de la liste
-void bubbleSortList(liste *p); // trie la liste
-void pickEltList(liste l,Elementliste *e,int index); // renvoie l'élément n° index
-bool belongEltList(liste p,Elementliste e); // true si l'élément est dans la liste
+/**
+ * @fn int nbEltList (liste lst)
+ * 
+ * @brief Nombre d'élément de la liste
+ *
+ * Calcule et renvoi le nombre d'élément de la liste "lst"
+ *
+ * @param lst Liste dont il faut calculer le nombre d'éléments
+ *
+ * @return Nombre d'éléments de la liste
+ */
+int nbEltList (liste lst);
+
+/**
+ * @fn void createList (liste *p)
+ *
+ * @brief Crée une liste statique
+ *
+ * Initialise la Tete et nbElt à 0 pour la liste "p"
+ *
+ * @param p Pointeur vers la liste à initialiser
+ */
+void createList (liste *p); 
+
+/**
+ * @fn void addFrontList (liste* p, Elementliste e)
+ *
+ * @brief Ajoute en début de liste
+ *
+ * Si la liste n'est pas pleine, ajoute un Elementliste "e"
+ * au début de la liste, incrémente le "nbElt" de la liste
+ * et met à jours la "Tete" de la liste
+ *
+ * @param p Pointeur vers la liste
+ * @param e Elementliste à ajouter à la fin de la liste
+ */
+void addFrontList (liste* p, Elementliste e);
+
+/**
+ * @fn void addTailList (liste* p, Elementliste e)
+ *
+ * @brief Ajoute en fin de liste
+ *
+ * Si la liste n'est pas pleine, ajoute un Elementliste "e"
+ * à la fin de la liste et incrémente le "nbElt" de la liste
+ *
+ * @param p Pointeur vers la liste
+ * @param e Elementliste à ajouter à la fin de la liste
+ */
+void addTailList (liste* p, Elementliste e); 
+
+/**
+ * @fn void delTailList (liste* p)
+ *
+ * @brief Supprime en fin de liste
+ *
+ * décrémente le "nbElt" de la liste "p"
+ * afin de supprimer le dernier Elementliste
+ *
+ * @param p Pointeur vers la liste
+ */
+void delTailList (liste* p);
+
+/**
+ * @fn void delFrontList (liste* p)
+ *
+ * @brief Supprime en début de liste
+ *
+ * Incrémente le "nbElt" et met à jour la "Tete" de la liste
+ * afin de supprimer le premier Elementliste
+ *
+ * @param p Pointeur vers la liste
+ */
+void delFrontList (liste* p); 
+
+/**
+ * @fn void headList (liste p, Elementliste* e)
+ *
+ * @brief Elementliste de tête
+ *
+ * Stocke dans "e" l' Elementliste de Tete de la liste "p"
+ *
+ * @param p Liste
+ * @param e Pointeur vers l' Elementliste qui contiendra l'élement de Tête
+ */
+void headList (liste p, Elementliste* e); 
+
+/**
+ * @fn void headList (liste p, Elementliste* e)
+ *
+ * @brief Elementliste de queue
+ *
+ * Stocke dans "e" l' Elementliste de Queue de la liste "p"
+ *
+ * @param p Liste
+ * @param e Pointeur vers l' Elementliste qui contiendra l'élement de Tête
+ */
+void tailList (liste p, Elementliste* e); 
+
+/**
+ * @fn bool emptyList (liste p)
+ *
+ * @brief Liste vide
+ *
+ * Vérifie que la liste soit bien vide
+ *
+ * @param p liste à analyser
+ *
+ * @return true si vide, false sinon
+ */
+bool emptyList (liste p);
+
+/**
+ * @fn bool fullList (liste p)
+ *
+ * @brief Liste pleine
+ *
+ * Vérifie que la liste soit bien pleine
+ *
+ * @param p Liste à analyser
+ *
+ * @return true si pleine, false sinon
+ */
+bool fullList (liste p); 
+
+/**
+ * @fn void dumpList (liste p, FILE* fp)
+ *
+ * @brief Affiche la liste
+ *
+ * Affiche le contenu de la liste "p",
+ * dans la destination d'affichage pointé par "fp"
+ *
+ * @param p Liste à afficher
+ * @param fp Destination d'affichage
+ */
+void dumpList (liste p, FILE* fp); 
+
+/**
+ * @fn void swapEltList (Elementliste* a, Elementliste* b)
+ *
+ * @brief Echange d' Elementliste dans la liste
+ *
+ * Echange deux Elementsliste "a" et "b" d'emplacement
+ *
+ * @param a Pointeur vers l'élement a à échanger
+ * @param b Pointeur vers l'élement b à échanger
+ */
+void swapEltList (Elementliste* a, Elementliste* b);
+
+/**
+ * @fn void bubbleSortList (liste* p)
+ *
+ * @brief Tri à bulle
+ *
+ * Tri les Elementsliste de la liste "p" en fonction de leurs poids.
+ * Suit la méthode du tri à bulle
+ *
+ * @param p Pointeur vers la liste à trier
+ */
+void bubbleSortList (liste* p); 
+
+/**
+ * @fn void pickEltList (liste l, Elementliste* e, int index)
+ *
+ * @brief Prend un Elementliste dans la liste
+ *
+ * Cherche l' Elementliste d'indice "index" dans la liste "l"
+ * et le stocke dans la structure pointé par "e"
+ *
+ * @param l Liste
+ * @param e Pointeur vers un Elementliste pour stocker l'élement
+ * @param index Indice de l'élement dans la liste
+ */
+void pickEltList (liste l, Elementliste* e, int index); 
+
+/**
+ * @fn bool belongEltList (liste p, Elementliste e)
+ *
+ * @brief Elementliste est dans liste
+ *
+ * Vérifie que l' Elementliste "e" soit bien présent dans la liste "p"
+ *
+ * @param p Liste
+ * @param e Elementliste qu'on cherche
+ *
+ * @return true si présent, false sinon
+ */
+bool belongEltList (liste p, Elementliste e);
+
 #endif
--- src/liste.c
+++ src/liste.c
@@ -1,11 +1,146 @@
+/**
+ * @file liste.c
+ *
+ * @brief Fichier de définitions relative aux listes
+ *
+ * Défini les fonctions de liste.h
+ *
+ * @author Dylan CARON
+ *
+ * @version 0.0.15
+ *
+ * @date 03/01/2019 
+ */
+
+
 #include "../head/liste.h"
 
-int nbEltList(liste lst) {
+
+int nbEltList (liste lst) {
   return lst.Tete;
 }
 
-void createList(liste *p) {
+
+void createList (liste *p) {
   p->nbElt = 0;
   p->Tete = 0;
-  p->TabListe
+}
+
+
+void addFrontList (liste* p, Elementliste e) {
+  if (! fullList(*p)) {
+    copie_element(p->Tabliste + ((p->Tete - 1) % DIMMAX), e);
+    p->Tete = (p->tete - 1) % DIMMAX;
+    p->nbElt ++;
+  }
+}
+
+
+void addTailList (liste* p, Elementliste e) {
+  if (! fullList(*p)) {
+    copie_element(p->Tabliste + ((p->Tete + p->nbElt) % DIMMAX), e);
+    p->nbElt ++;
+  }
+}
+
+
+void delTailList (liste* p) {
+  if (! emptyList(*p)) {
+    p->nbElt --;
+  }
+}
+
+
+void delFrontList (liste* p) {
+  if (! emptyList(*p)) {
+    p->Tete = (p->Tete + 1) % DIMMAX;
+    p->nbElt --;
+  }
+}
+
+
+void headList (liste p, Elementliste* e) {
+  if (! emptyList(p)) {
+    copie_element(e, p.Tabliste[p.Tete]);
+  }
+}
+
+
+void tailList (liste p, Elementliste* e) {
+  if (! emptyList(p)) {
+    copie_element(e, p.Tabliste[(p.Tete + p.nbElt) % DIMMAX]);
+  }
+}
+
+
+bool emptyList (liste p) {
+  if (p.nbElt == 0) {
+    return true;
+  }
+
+  return false;
+}
+
+
+bool fullList (liste p) {
+  if(p.nbElt == DIMMAX) {
+    return true;
+  }
+
+  return false;
+}
+
+
+void dumpList (liste p, FILE* fp) {
+  for (i = 0; i < p.nbElt - 1; i ++) {
+    afficher_element(p.Tabliste[(p.Tete + i) % DIMMAX], fp);
+    fprintf(fp, ",\n");
+  }
+
+  afficher_element(p.Tabliste[(p.Tete + p.nbElt - 1) % DIMMAX], fp);
+  fprintf(fp, "\n");
+}
+
+
+void swapEltList (Elementliste* a, Elementliste* b) {
+  Elementliste buff;
+  
+  copie_element(&buff, *a);
+  copie_element(a, *b);
+  copie_element(b, buff);
+}
+
+
+void bubbleSortList (liste* p) {a
+  bool sorted = true;
+
+  for (int i = nbEltList(*p) - 1; i > 0; i --) {
+    sorted = true;
+    for (int j = 0; j < i; j ++) {
+      if (p->Tabliste[(p->Tete + i) % DIMMAX].poids  < p->Tabliste[(p->Tete + i + 1) % DIMMAX].poids) {
+        swapEltList(p->Tabliste + ((p->Tete + i) % DIMMAX), p->Tabliste + ((p->Tete + i + 1) % DIMMAX));
+        sorted = false;
+      }
+    }
+
+    if (sorted) return;
+  }
+}
+
+
+void pickEltList (liste l, Elementliste* e, int index) {
+  if (index < l.nbElt) {
+    copie_element(e, l.Tabliste[(l.Tete + index) % DIMMAX]);
+  }
+}
+
+
+bool belongEltList (liste p, Elementliste e) {
+  for (int i = 0; i < p.nbElt; i ++) {
+    if (cmp_elements(e, p.Tabliste[(p.Tete + i) % DIMMAX])) {
+      return true;
+    }
+  }
+
+  return false;
 }
